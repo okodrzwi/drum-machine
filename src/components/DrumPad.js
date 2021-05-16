358fc6d10edb6de1d3730cdda9180dc098e7da87
okodrzwi <1krzysztof.kokoszka@gmail.com>
2021-05-16 16:33:03 +0100

    Prettier

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,40 +1,45 @@
 import React from 'react';
 
-class DrumPad extends React.Component  {
+class DrumPad extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
     this.handleKey = this.handleKey.bind(this);
     this.playSound = this.playSound.bind(this);
-  };
-  componentDidMount() { document.addEventListener("keydown", this.handleKey); }
-  componentWillUnmount() { document.removeEventListener("keydown", this.handleKey) };
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKey);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKey);
+  }
 
   handleKey(event) {
-    if (event.keyCode==this.props.keyCode) {     
+    if (event.keyCode == this.props.keyCode) {
       this.playSound();
     }
-  };  
+  }
   playSound() {
     const sound = document.getElementById(this.props.keyTrigger);
     sound.play();
     this.props.updateDisplay(this.props.name);
-  };
+  }
   render() {
-    return (    
-      <div className="drum-pad"
+    return (
+      <div
+        className="drum-pad"
         id={this.props.keyCode}
         onClick={this.playSound}
-        soundName= {this.props.soundName}        
+        soundName={this.props.soundName}
       >
-       
         {this.props.keyTrigger}
-        <audio className='clip'
+        <audio
+          className="clip"
           id={this.props.keyTrigger}
           src={this.props.source}
         />
-      </div>   
-    )
+      </div>
+    );
   }
 }
 export default DrumPad;

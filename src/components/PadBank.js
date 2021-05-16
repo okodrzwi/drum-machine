61312265f7caefae91fe2729c096974000cd4423
okodrzwi <1krzysztof.kokoszka@gmail.com>
2021-05-16 16:31:54 +0100

    Added basic CSS

--- src/components/PadBank.js
+++ src/components/PadBank.js
@@ -1,35 +1,25 @@
 import React from 'react';
-import DrumPad from "./DrumPad"
+import DrumPad from './DrumPad';
 
 class PadBank extends React.Component {
   constructor(props) {
-    super(props);  
+    super(props);
   }
 
   render() {
     let padBank;
-              padBank = this.props.padBank.map((clip, index, soundsArray) => {
-              return (
-                <DrumPad 
-                  keyTrigger={soundsArray[index].keyTrigger} 
-                  source={soundsArray[index].source}
-                  keyCode={soundsArray[index].keyCode}
-                  name={soundsArray[index].name}
-                  updateDisplay={this.props.updateDisplay}
-                >
-                </DrumPad>
-                
-              )
-            },
-            )
-      return (        
-          <div id="drum-machine">
-          {padBank}
-
-          
-        </div>
+    padBank = this.props.padBank.map((clip, index, soundsArray) => {
+      return (
+        <DrumPad
+          keyTrigger={soundsArray[index].keyTrigger}
+          source={soundsArray[index].source}
+          keyCode={soundsArray[index].keyCode}
+          name={soundsArray[index].name}
+          updateDisplay={this.props.updateDisplay}
+        ></DrumPad>
       );
-    }
-  
-};
+    });
+    return <div id="drum-machine">{padBank}</div>;
+  }
+}
 export default PadBank;
